ad9b4db62007e4bd64f6259282ef310184d60c67
Himanshujadhav2004 <himanshujadhav341@gmail.com>
2025-07-19 11:24:25 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { LoginForm } from "@/components/login-form";
+
+export default function Home() {
+  return (
+    <div className="justify-items-center align-items-center flex min-h-svh w-full items-center justify-center ">
+
+
+    </div>
+  );
+}
---
+++ src/components/singup-form.tsx
@@ -0,0 +1,74 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import Link from 'next/link';
+
+export function SignupForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Create Your Account</CardTitle>
+          <CardDescription>
+            Enter your email below to create a new account.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Confirm Password</Label>
+                
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                Sign Up
+                </Button>
+                <Button variant="outline" className="w-full">
+                 Sign Up with Google
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+            Have an account?{" "}
+            <Link href="/login" passHref>
+      <span className="underline underline-offset-4">Login</span>
+    </Link>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
